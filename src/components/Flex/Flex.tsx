b4132989b163e294762f018bfbde7d58fc9f2fa4
Andy Wright <atwright147@gmail.com>
2024-12-14 23:14:19 +0000

    initial commit

---
+++ src/components/Flex/Flex.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+interface Props {
+  // Define your props here
+}
+
+export const Flex: FC<Props> = ({ /* props */ }) => {
+  return (
+    <div>
+      {/* Your component implementation */}
+    </div>
+  );
+};
