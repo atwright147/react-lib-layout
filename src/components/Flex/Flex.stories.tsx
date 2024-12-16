7e5d2b1a512ab0b028e7be38cf567109af210c98
Andy Wright <atwright147@gmail.com>
2024-12-16 22:43:13 +0000

    add stories for Flex

---
+++ src/components/Flex/Flex.stories.tsx
@@ -0,0 +1,59 @@
+// src/components/Flex/Flex.stories.tsx
+
+import { Flex, type Props } from './Flex';
+
+const boxStyles = {
+  outline: '1px solid red',
+  width: '100px',
+};
+
+export default {
+  component: Flex,
+  title: 'Flex',
+  tags: ['autodocs'],
+  args: {
+    children: (
+      <>
+        <div id="box1" style={boxStyles}>
+          1
+        </div>
+        <div id="box2" style={boxStyles}>
+          2
+        </div>
+        <div id="box3" style={boxStyles}>
+          3
+        </div>
+      </>
+    ),
+  },
+  argTypes: {
+    flexDirection: {
+      options: ['row', 'column'],
+      control: { type: 'select' },
+    },
+    justifyContent: {
+      options: ['flex-start', 'center', 'space-between'],
+      control: { type: 'select' },
+    },
+    alignItems: {
+      options: ['flex-start', 'center', 'flex-end'],
+      control: { type: 'select' },
+    },
+    gap: {
+      control: { type: 'number' },
+    },
+  },
+};
+
+export const Default = (args: Props) => <Flex {...args} />;
+export const Row = (args: Props) => <Flex flexDirection="row" {...args} />;
+export const Column = (args: Props) => (
+  <Flex flexDirection="column" {...args} />
+);
+export const JustifyCenter = (args: Props) => (
+  <Flex justifyContent="center" {...args} />
+);
+export const AlignCenter = (args: Props) => (
+  <Flex alignItems="center" {...args} />
+);
+export const Gap = (args: Props) => <Flex gap={10} {...args} />;
--- src/components/Flex/Flex.tsx
+++ src/components/Flex/Flex.tsx
@@ -2,7 +2,7 @@ import { type ComponentPropsWithRef, forwardRef } from 'react';
 import { useFlex } from '../../hooks/useFlex';
 import type { FlexProps } from '../../types/flex';
 
-type Props = ComponentPropsWithRef<'div'> & FlexProps;
+export type Props = ComponentPropsWithRef<'div'> & FlexProps;
 
 export const Flex = forwardRef<HTMLDivElement, Props>(
   ({ children, ...props }, ref) => {
